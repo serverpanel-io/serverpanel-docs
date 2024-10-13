873309d634750c33d25e86dbd9ac78c6d2a9ea0f
rivo.t <rivo.tuksammel@gmail.com>
2024-10-13 07:59:20 +0000

    Update.

---
+++ serverpanel-docs/blog/2024-10-13-hello-world.md
@@ -0,0 +1,7 @@
+---
+slug: hello-world
+title: First Devblog Post
+authors: [rivo]
+tags: [serverpanel]
+---
+Hello, world!
--- serverpanel-docs/docusaurus.config.ts
+++ serverpanel-docs/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/serverpanel-io/serverpanel-docs/tree/main/serverpanel-docs',
         },
         blog: {
           showReadingTime: true,
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/serverpanel-io/serverpanel-docs/tree/main/serverpanel-docs',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
